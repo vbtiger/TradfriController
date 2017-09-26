491c110d7e6b2bf4a7dfd168850bb7777eec39ff
vbtiger <varga.balazs.official@gmail.com>
2017-09-26 20:47:50 +0200

    Project init

---
+++ TradfriLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TradfriLib")]
+[assembly: AssemblyDescription("IKEA Tradfri Light Controller Core")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tiger Products")]
+[assembly: AssemblyProduct("TradfriLib")]
+[assembly: AssemblyCopyright("Copyright © vbtiger 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("423fcf1a-5ef0-41fe-b180-9c4eeb78544c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
