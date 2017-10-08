896d2d8277943b5bfed01bf0a16918f3f3b0ae01
vbtiger <varga.balazs.official@gmail.com>
2017-10-08 23:12:19 +0200

    Add console app for testing

---
+++ LightTesterApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using TradfriLib;
+using TradfriLib.Data;
+
+namespace LightTesterApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
